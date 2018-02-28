767c0481a31aa7ca5fa80047d9c5743aa90cdabd
Masashi Hirano <cherrydog07@gmail.com>
2018-02-28 23:23:25 +0900

    Improve test with jest

--- test/SlideShow.test.js
+++ test/SlideShow.test.js
@@ -1,4 +1,3 @@
-import test from 'ava';
 import React from 'react';
 import {configure, shallow} from 'enzyme';
 import SlideShow from '../src/SlideShow';
@@ -6,7 +5,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
 
-test("props style", t => {
+test("props style", () => {
   const props = {
     images: [
       "static/test/page1",
@@ -20,14 +19,14 @@ test("props style", t => {
       {...props}
     />
   );
-  t.is(wrapper.find("div").at(0).prop("style").width, 100);
+  expect(wrapper.find("div").at(0).prop("style").width).toEqual(100);
   props.style = Object.assign({}, props.style, {width: 80});
   wrapper.setProps(props);
   // style object must be immutable
-  t.is(wrapper.find("div").at(0).prop("style").width, 100);
+  expect(wrapper.find("div").at(0).prop("style").width).toEqual(100);
 });
 
-test("props images", t => {
+test("props images", () => {
   const wrapper = shallow(
     <SlideShow
       images={[
@@ -38,27 +37,26 @@ test("props images", t => {
       style={{fontWeight: "bold"}}
     />
   );
-  t.truthy(wrapper);
-  t.is(wrapper.state().src, "static/test/page1");
-  t.is(wrapper.state().index, 0);
+  expect(wrapper.state().src).toEqual("static/test/page1");
+  expect(wrapper.state().index).toEqual(0);
 });
-test("componentWillMount props.src is undefined", t => {
+test("componentWillMount props.images is undefined", () => {
   const wrapper = shallow(
     <SlideShow/>
   );
-  t.is(wrapper.state().src, "");
+  expect(wrapper.state().src).toEqual("");
 });
 
-test("componentWillMount props.src is empty", t => {
+test("componentWillMount props.images is empty", () => {
   const wrapper = shallow(
     <SlideShow images={[]}/>
   );
-  t.is(wrapper.state().src, "");
+  expect(wrapper.state().src).toEqual("");
 });
 
-test("componentWillMount props.src is null", t => {
+test("componentWillMount props.images is null", () => {
   const wrapper = shallow(
     <SlideShow images={null}/>
   );
-  t.is(wrapper.state().src, "");
+  expect(wrapper.state().src).toEqual("");
 });
