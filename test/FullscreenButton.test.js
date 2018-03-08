752e7a84329d2039a1651aed2edd509f6b4ed275
Masashi Hirano <cherrydog07@gmail.com>
2018-03-09 00:53:29 +0900

    Add: test for FullscreenButton

---
+++ test/FullscreenButton.test.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {configure, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
+import FullscreenButton from '../src/FullscreenButton';
+
+configure({adapter: new Adapter()});
+
+describe('FullscreenButton', () => {
+  test('click event', () => {
+    const spy = sinon.spy();
+    const wrapper = mount(
+      <FullscreenButton onClick={spy}/>
+    );
+    expect(spy.called).toBeFalsy();
+    wrapper.find('button').simulate('click');
+    wrapper.find('button').simulate('click');
+    wrapper.find('button').simulate('click');
+    expect(spy.callCount).toBe(3);
+  })
+});
