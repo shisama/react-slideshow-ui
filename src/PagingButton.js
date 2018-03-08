e921616bb4cba94397670ba63c13b2475716293e
Masashi Hirano <cherrydog07@gmail.com>
2018-03-08 23:12:49 +0900

    Refactor: replace Bar to PagingButton
    Fix not to keep running click event

---
+++ src/PagingButton.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {pure} from 'recompose';
+import styles from './styles';
+
+export default pure(function({onClick, children}) {
+  return (
+    <React.Fragment>
+      <button onClick={onClick} style={styles.BUTTON}>
+        {children}
+      </button>
+    </React.Fragment>
+  );
+});
