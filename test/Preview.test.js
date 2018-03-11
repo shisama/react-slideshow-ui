8e401b39512068d86459dc5dcc8f6471df84ea72
Masashi Hirano <cherrydog07@gmail.com>
2018-03-11 10:49:14 +0900

    Add: test for Preview

---
+++ test/Preview.test.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {configure, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Preview from '../src/Preview';
+
+configure({adapter: new Adapter()});
+
+describe("Preview", () => {
+  test("preview index props", () => {
+    const wrapper = mount(
+      <Preview
+        images={[
+          "test/image1.png",
+          "test/image2.png",
+          "test/image3.png"
+        ]}
+        previewIndex={0}
+      />
+    );
+    expect(wrapper.find('p').text()).toBe('1 / 3');
+
+    wrapper.setProps({previewIndex: 1});
+    expect(wrapper.find('p').text()).toBe('2 / 3');
+
+    wrapper.setProps({previewIndex: 2});
+    expect(wrapper.find('p').text()).toBe('3 / 3');
+  });
+});
