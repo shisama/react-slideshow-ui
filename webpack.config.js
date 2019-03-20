d045ca60df164374e7cd3f4f204638bd83c38d98
Masashi Hirano <cherrydog07@gmail.com>
2019-03-20 23:34:06 +0900

    Improve webpack build performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,43 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    'slideshow': [
-      path.resolve(__dirname, 'src/demo/index.tsx')
-    ]
+    slideshow: [path.resolve(__dirname, "src/demo/index.tsx")]
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'demo'),
-    publicPath: '/',
+    filename: "[name].js",
+    path: path.resolve(__dirname, "demo"),
+    publicPath: "/"
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
     new webpack.DllReferencePlugin({
       context: __dirname,
-      manifest: require("./dll/vendor-manifest.json"),
-    }),
+      manifest: require("./dll/vendor-manifest.json")
+    })
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: [
+          { loader: "cache-loader" },
+          { loader: "thread-loader" },
+          {
+            loader: "ts-loader",
+            options: {
+              happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx'],
-  },
+    extensions: [".ts", ".tsx"]
+  }
 };
